6330ddc65dd08d9742b0e0f6cf653769479389d5
andrewsnowflaketrial <139216548+andrewsnowflaketrial@users.noreply.github.com>
2023-07-13 10:11:54 +0100

    Update streamlit_app.py
    changed text and header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,7 @@ streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('healthy thing 1') 
-streamlit.text('healthy thing 2')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
